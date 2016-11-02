29bb6b563cdafef2e4656238ffb8f651081dbd29
CAOZHENJIE2O16 <zhenjiecaoaccnt@gmail.com>
2016-11-02 15:19:37 -0400

    Add files via upload
    src

---
+++ src/main/java/com/example/caozhenjie/ec601_spotify/ImlHandWave.java
@@ -0,0 +1,16 @@
+package com.example.caozhenjie.ec601_spotify;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import org.opencv.android.OpenCVLoader;
+
+public class ImlHandWave extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_iml_hand_wave);
+    }
+}
---
+++ src/main/java/com/example/caozhenjie/ec601_spotify/SecondOne.java
@@ -0,0 +1,73 @@
+package com.example.caozhenjie.ec601_spotify;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import android.widget.ImageButton;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class SecondOne extends AppCompatActivity {
+
+    private Button btn1, btn2;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second_one);
+        initView();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+
+    private void initView() {
+        btn1 = (Button) findViewById(R.id.btn1);
+        btn2 = (Button) findViewById(R.id.btn2);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                btn1.setBackgroundResource(R.drawable.itunes);
+                Toast.makeText(SecondOne.this, "btn1", Toast.LENGTH_SHORT).show();
+            }
+
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn2.setBackgroundResource(R.drawable.spotify);
+                Intent intent = new Intent(SecondOne.this, spotifyloginpage.class);
+                startActivity(intent);
+
+
+                Toast.makeText(SecondOne.this, "btn2", Toast.LENGTH_SHORT).show();
+            }
+
+        });
+    }
+
+
+
+
+}
+
